347ced5aa1df85d27efaf547b1a61bdb46756b12
Rafaela de Albuquerque <rafaelaa170799@gmail.com>
2024-10-24 15:33:36 -0300

    Autenticação

--- src/main/java/com/accept/services/EmployeeService.java
+++ src/main/java/com/accept/services/EmployeeService.java
@@ -43,31 +43,11 @@ public class EmployeeService {
     }
 
     @Transactional(readOnly = true)
-    public List<EmployeeDTO> getAllEmployees() {
-        List<Employee> employees = employeeRepository.findAll();
-        if (employees.isEmpty()) {
-            throw new EntityNotFoundException("No employees found.");
-        }
-        return employees.stream()
-                .map(employee -> modelMapper.map(employee, EmployeeDTO.class))
-                .collect(Collectors.toList());
-    }
-
-    @Transactional(readOnly = true)
     public EmployeeDTO getEmployeeById(UUID employeeId) {
         Employee employee = employeeRepository.findById(employeeId)
                 .orElseThrow(() -> new EntityNotFoundException("Employee not found: " + employeeId));
         return modelMapper.map(employee, EmployeeDTO.class);
     }
-
-//    @Transactional
-//    public EmployeeDTO createEmployee(EmployeeDTO employeeDTO) {
-//        validateEmployee(employeeDTO);
-//        Employee employee = modelMapper.map(employeeDTO, Employee.class);
-//        employee.onCreate();
-//        Employee savedEmployee = employeeRepository.save(employee);
-//        return modelMapper.map(savedEmployee, EmployeeDTO.class);
-//    }
 
     public Optional<Employee> registerEmployee(Employee employee) {
 
@@ -80,13 +60,13 @@ public class EmployeeService {
 
     }
 
-    public Optional<Employee> updateEmployee (Employee employee) {
+    public Optional<Employee> updateEmployee(Employee employee) {
 
-        if(employeeRepository.findById(employee.getEmployeeId()).isPresent()) {
+        if (employeeRepository.findById(employee.getEmployeeId()).isPresent()) {
 
             Optional<Employee> employeeSearch = employeeRepository.findByEmployeeEmail(employee.getEmployeeEmail());
 
-            if ( (employeeSearch.isPresent()) && ( employeeSearch.get().getEmployeeId() != employee.getEmployeeId()))
+            if ((employeeSearch.isPresent()) && (employeeSearch.get().getEmployeeId() != employee.getEmployeeId()))
                 throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "employee already exists!", null);
 
             employee.setPassword(criptografarSenha(employee.getPassword()));
@@ -99,31 +79,24 @@ public class EmployeeService {
 
     }
 
-    public Optional<EmployeeDTO> authenticateEmployee (Optional<EmployeeDTO> employeeDTO) {
+    public Optional<EmployeeDTO> authenticateEmployee(Optional<EmployeeDTO> employeeDTO) {
 
-        // Gera o Objeto de autenticação
         var credenciais = new UsernamePasswordAuthenticationToken(employeeDTO.get().getEmployeeEmail(), employeeDTO.get().getPassword());
 
-        // Autentica o Usuario
         Authentication authentication = authenticationManager.authenticate(credenciais);
 
-        // Se a autenticação foi efetuada com sucesso
         if (authentication.isAuthenticated()) {
 
-            // Busca os dados do usuário
             Optional<Employee> employee = employeeRepository.findByEmployeeEmail(employeeDTO.get().getEmployeeEmail());
 
-            // Se o usuário foi encontrado
             if (employee.isPresent()) {
 
-                // Preenche o Objeto usuarioLogin com os dados encontrados
                 employeeDTO.get().setEmployeeId(employee.get().getEmployeeId());
                 employeeDTO.get().setEmployeeName(employee.get().getEmployeeName());
                 employeeDTO.get().setPosition(employee.get().getPosition());
                 employeeDTO.get().setToken(gerarToken(employeeDTO.get().getEmployeeEmail()));
                 employeeDTO.get().setPassword("");
 
-                // Retorna o Objeto preenchido
                 return employeeDTO;
 
             }
@@ -133,6 +106,28 @@ public class EmployeeService {
         return Optional.empty();
 
     }
+
+    @Transactional(readOnly = true)
+    public List<EmployeeDTO> getAllEmployees() {
+        List<Employee> employees = employeeRepository.findAll();
+
+        if (employees.isEmpty()) {
+            throw new EntityNotFoundException("No employees found.");
+        }
+
+        return employees.stream()
+                .map(employee -> modelMapper.map(employee, EmployeeDTO.class))
+                .collect(Collectors.toList());
+    }
+
+    public void deleteEmployee(UUID id) {
+        if (employeeRepository.existsById(id)) {
+            employeeRepository.deleteById(id);
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuário não encontrado!");
+        }
+    }
+
 
     private String criptografarSenha(String senha) {
 
@@ -144,29 +139,6 @@ public class EmployeeService {
 
     private String gerarToken(String usuario) {
         return "Bearer " + jwtService.generateToken(usuario);
-    }
-
-//    @Transactional
-//    public EmployeeDTO updateEmployee(UUID employeeId, EmployeeDTO employeeDTO) {
-//        Employee employee = employeeRepository.findById(employeeId)
-//                .orElseThrow(() -> new EntityNotFoundException("Employee not found: " + employeeId));
-//        employee.setEmployeeName(employeeDTO.getEmployeeName());
-//        employee.setEmployeeEmail(employeeDTO.getEmployeeEmail());
-//        employee.setPassword(employeeDTO.getPassword());
-//        employee.setPosition(employeeDTO.getPosition());
-//        employee.onUpdate();
-//        Employee updatedEmployee = employeeRepository.save(employee);
-//        return modelMapper.map(updatedEmployee, EmployeeDTO.class);
-//    }
-
-    @Transactional
-    public void deleteEmployee(UUID employeeId) {
-        Optional<Employee> employeeOptional = employeeRepository.findById(employeeId);
-        if (employeeOptional.isPresent()) {
-            employeeRepository.delete(employeeOptional.get());
-        } else {
-            throw new EntityNotFoundException("Student not found with id: " + employeeId);
-        }
     }
 
     private void validateEmployee(EmployeeDTO employeeDTO) {
