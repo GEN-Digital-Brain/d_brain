8b642590345e04ac9823f92fb279746cf3293709
91zacDev <zaq.c12@gmail.com>
2024-10-23 22:05:23 -0300

    Updated EmployeerController with JWT changes

--- src/main/java/com/accept/controllers/EmployeeController.java
+++ src/main/java/com/accept/controllers/EmployeeController.java
@@ -1,65 +1,79 @@
 package com.accept.controllers;
 
-import com.accept.dto.EmployeeDTO;
-import com.accept.services.EmployeeService;
-import jakarta.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.accept.dto.EmployeeDTO;
+import com.accept.services.EmployeeService;
+
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-
-import java.util.List;
-import java.util.UUID;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
 
 @Validated
 @RestController
-@RequestMapping("employees")
+@RequestMapping("/employees")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @Tag(name = "Employees Controller", description = "REST API for managing employees")
 public class EmployeeController {
 
     @Autowired
-    private final EmployeeService employeeService;
+    private EmployeeService employeeService;
 
-    public EmployeeController(EmployeeService employeeService) {
-        this.employeeService = employeeService;
-    }
-
-    @GetMapping
+    @GetMapping("/all")
     @Operation(summary = "Get all Employees", description = "Retrieve a list of all employees")
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "Successful operation", content = @Content(mediaType = "application/json", schema = @Schema(implementation = EmployeeDTO.class))),
-            @ApiResponse(responseCode = "500", description = "Internal server error")
+        @ApiResponse(responseCode = "200", description = "Successful operation",
+                content = @Content(mediaType = "application/json", schema = @Schema(implementation = EmployeeDTO.class))),
+        @ApiResponse(responseCode = "500", description = "Internal server error")
     })
     public ResponseEntity<List<EmployeeDTO>> getAll() {
         List<EmployeeDTO> employees = employeeService.getAllEmployees();
         return ResponseEntity.ok(employees);
     }
 
-    @GetMapping("{id}")
+    @GetMapping("/{id}")
     @Operation(summary = "Get Employee by ID", description = "Retrieve a specific employee by their ID")
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "Successful operation", content = @Content(mediaType = "application/json", schema = @Schema(implementation = EmployeeDTO.class))),
-            @ApiResponse(responseCode = "404", description = "Employee not found"),
-            @ApiResponse(responseCode = "500", description = "Internal server error")
+        @ApiResponse(responseCode = "200", description = "Successful operation",
+                content = @Content(mediaType = "application/json", schema = @Schema(implementation = EmployeeDTO.class))),
+        @ApiResponse(responseCode = "404", description = "Employee not found"),
+        @ApiResponse(responseCode = "500", description = "Internal server error")
     })
     public ResponseEntity<EmployeeDTO> getById(@PathVariable UUID id) {
-        EmployeeDTO employee = employeeService.getEmployeeById(id);
-        return ResponseEntity.ok(employee);
+        Optional<EmployeeDTO> employee = Optional.ofNullable(employeeService.getEmployeeById(id));
+        return employee.map(ResponseEntity::ok)
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 
-    @PostMapping
+    @PostMapping("/create")
     @Operation(summary = "Create an Employee", description = "Creates a new employee with the provided details")
     @ApiResponses({
-            @ApiResponse(responseCode = "201", description = "Employee created", content = @Content(mediaType = "application/json", schema = @Schema(implementation = EmployeeDTO.class))),
-            @ApiResponse(responseCode = "400", description = "Bad request"),
-            @ApiResponse(responseCode = "500", description = "Internal server error")
+        @ApiResponse(responseCode = "201", description = "Employee created",
+                content = @Content(mediaType = "application/json", schema = @Schema(implementation = EmployeeDTO.class))),
+        @ApiResponse(responseCode = "400", description = "Bad request"),
+        @ApiResponse(responseCode = "500", description = "Internal server error")
     })
     public ResponseEntity<EmployeeDTO> create(@Valid @RequestBody EmployeeDTO employeeDTO) {
         EmployeeDTO createdEmployee = employeeService.createEmployee(employeeDTO);
@@ -69,26 +83,36 @@ public class EmployeeController {
     @PutMapping("{id}")
     @Operation(summary = "Update an Employee", description = "Update the details of an existing employee")
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "Employee updated", content = @Content(mediaType = "application/json", schema = @Schema(implementation = EmployeeDTO.class))),
-            @ApiResponse(responseCode = "404", description = "Employee not found"),
-            @ApiResponse(responseCode = "400", description = "Bad request"),
-            @ApiResponse(responseCode = "500", description = "Internal server error")
+        @ApiResponse(responseCode = "200", description = "Employee updated",
+                content = @Content(mediaType = "application/json", schema = @Schema(implementation = EmployeeDTO.class))),
+        @ApiResponse(responseCode = "404", description = "Employee not found"),
+        @ApiResponse(responseCode = "401", description = "Unauthorized - Invalid Token"),
+        @ApiResponse(responseCode = "400", description = "Bad request"),
+        @ApiResponse(responseCode = "500", description = "Internal server error")
     })
-    public ResponseEntity<EmployeeDTO> update(@PathVariable UUID id, @Valid @RequestBody EmployeeDTO employeeDTO) {
-        EmployeeDTO updatedEmployee = employeeService.updateEmployee(id, employeeDTO);
+    public ResponseEntity<EmployeeDTO> update(
+            @PathVariable UUID id,
+            @Valid @RequestBody EmployeeDTO employeeDTO,
+            @RequestHeader("Authorization") String token // Adicionando o token JWT como Header
+    ) {
+        EmployeeDTO updatedEmployee = employeeService.updateEmployee(id, employeeDTO, token);
         return ResponseEntity.ok(updatedEmployee);
     }
 
-    @DeleteMapping("{id}")
+    @DeleteMapping("/delete/{id}")
     @Operation(summary = "Delete an Employee", description = "Deletes a specific employee by their ID")
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "Employee deleted successfully"),
-            @ApiResponse(responseCode = "404", description = "Employee not found"),
-            @ApiResponse(responseCode = "500", description = "Internal server error")
+        @ApiResponse(responseCode = "200", description = "Employee deleted successfully"),
+        @ApiResponse(responseCode = "404", description = "Employee not found"),
+        @ApiResponse(responseCode = "500", description = "Internal server error")
     })
     public ResponseEntity<String> delete(@PathVariable UUID id) {
-        employeeService.deleteEmployee(id);
-        String message = "Employee with ID " + id + " deleted successfully.";
-        return ResponseEntity.status(HttpStatus.OK).body(message);
+        try {
+            employeeService.deleteEmployee(id);
+            String message = "Employee with ID " + id + " deleted successfully.";
+            return ResponseEntity.status(HttpStatus.OK).body(message);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Employee not found.");
+        }
     }
 }
